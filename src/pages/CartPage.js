c72b0daa61db3934f8548838dff53e88cc335ec5
Franco <franco@Francos-MacBook-Air.local>
2020-05-03 01:41:09 +0200

    change cartPage to cart

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Hero from "../components/Hero";
 import CartBcg from "../images/storeBcg.jpeg";
-import CartSection from "../components/CartPage";
+import CartSection from "../components/CartPage/Cart";
 
 export default function CartPage() {
   return (
